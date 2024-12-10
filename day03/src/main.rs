8109b1ecd7138a5ef4db109aa11610c302f2ad7f
paul <tabatabai@protonmail.com>
2024-12-10 22:56:24 +0100

    day 3 part 1

---
+++ day03/src/main.rs
@@ -0,0 +1,21 @@
+use std::fs;
+use regex::Regex;
+
+fn main() {
+    let input = fs::read_to_string("./src/input.txt").expect("input exists");
+    // let re = Regex::new(r"^mul(\d+,\d+)$").unwrap();
+    let re = Regex::new(r"mul\(\d+\,\d+\)").unwrap();
+    let number_re = Regex::new(r"\d+").unwrap();
+    let captures = re.captures_iter(&input);
+    let mut total: i32 = 0;
+    for d in captures {
+        println!("{}", &d[0]);
+        let mut new_term: i32 = 1;
+        let number_captures = number_re.captures_iter(&d[0]);
+        for x in number_captures {
+            new_term *= &x[0].parse().expect("parsable");
+        }
+        total += new_term;
+    }
+    println!("Day 3, part 1: {total}")
+}
