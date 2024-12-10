c36270103ee8d1e2914b28103a71f34a214ff7ff
paul <tabatabai@protonmail.com>
2024-12-10 23:40:03 +0100

    experimenting day 04

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -16,8 +16,36 @@ fn main() {
     let n = matrix.len();
     for i in 0..n {
         for j in 0..n {
-            if matrix[i][j] == "X" {
-                total += 1;
+
+            if i + 3 <= n - 1 {
+                let mut maybe_xmas = String::new();
+                maybe_xmas.push_str(&matrix[i][j]);
+                maybe_xmas.push_str(&matrix[i+1][j]);
+                maybe_xmas.push_str(&matrix[i+2][j]);
+                maybe_xmas.push_str(&matrix[i+3][j]);
+                if maybe_xmas == "XMAS" {
+                    total += 1
+                }
+            }
+            if j + 3 <= n - 1 {
+                let mut maybe_xmas = String::new();
+                maybe_xmas.push_str(&matrix[i][j]);
+                maybe_xmas.push_str(&matrix[i][j+1]);
+                maybe_xmas.push_str(&matrix[i][j+2]);
+                maybe_xmas.push_str(&matrix[i][j+3]);
+                if maybe_xmas == "XMAS" {
+                    total += 1
+                }
+            }
+            if ( i + 3 <= n - 1 ) && (j + 3 <= n - 1) {
+                let mut maybe_xmas = String::new();
+                maybe_xmas.push_str(&matrix[i][j]);
+                maybe_xmas.push_str(&matrix[i+1][j+1]);
+                maybe_xmas.push_str(&matrix[i+2][j+2]);
+                maybe_xmas.push_str(&matrix[i+3][j+3]);
+                if maybe_xmas == "XMAS" {
+                    total += 1
+                }
             }
         }
     }
