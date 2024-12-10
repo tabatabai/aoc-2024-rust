8f56f3742a07356cbcc0e58a866f4c634094dac2
paul <tabatabai@protonmail.com>
2024-12-10 23:40:18 +0100

    formatting changes day 4

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let input = fs::read_to_string("./src/input.txt").expect("input exists");
     let line_iter = input.lines();
     let mut matrix: Vec<Vec<String>> = Vec::new();
-    for line in line_iter{
+    for line in line_iter {
         let mut row: Vec<String> = Vec::new();
         for char in line.chars() {
             row.push(char.to_string());
@@ -16,13 +16,12 @@ fn main() {
     let n = matrix.len();
     for i in 0..n {
         for j in 0..n {
-
             if i + 3 <= n - 1 {
                 let mut maybe_xmas = String::new();
                 maybe_xmas.push_str(&matrix[i][j]);
-                maybe_xmas.push_str(&matrix[i+1][j]);
-                maybe_xmas.push_str(&matrix[i+2][j]);
-                maybe_xmas.push_str(&matrix[i+3][j]);
+                maybe_xmas.push_str(&matrix[i + 1][j]);
+                maybe_xmas.push_str(&matrix[i + 2][j]);
+                maybe_xmas.push_str(&matrix[i + 3][j]);
                 if maybe_xmas == "XMAS" {
                     total += 1
                 }
@@ -30,19 +29,19 @@ fn main() {
             if j + 3 <= n - 1 {
                 let mut maybe_xmas = String::new();
                 maybe_xmas.push_str(&matrix[i][j]);
-                maybe_xmas.push_str(&matrix[i][j+1]);
-                maybe_xmas.push_str(&matrix[i][j+2]);
-                maybe_xmas.push_str(&matrix[i][j+3]);
+                maybe_xmas.push_str(&matrix[i][j + 1]);
+                maybe_xmas.push_str(&matrix[i][j + 2]);
+                maybe_xmas.push_str(&matrix[i][j + 3]);
                 if maybe_xmas == "XMAS" {
                     total += 1
                 }
             }
-            if ( i + 3 <= n - 1 ) && (j + 3 <= n - 1) {
+            if (i + 3 <= n - 1) && (j + 3 <= n - 1) {
                 let mut maybe_xmas = String::new();
                 maybe_xmas.push_str(&matrix[i][j]);
-                maybe_xmas.push_str(&matrix[i+1][j+1]);
-                maybe_xmas.push_str(&matrix[i+2][j+2]);
-                maybe_xmas.push_str(&matrix[i+3][j+3]);
+                maybe_xmas.push_str(&matrix[i + 1][j + 1]);
+                maybe_xmas.push_str(&matrix[i + 2][j + 2]);
+                maybe_xmas.push_str(&matrix[i + 3][j + 3]);
                 if maybe_xmas == "XMAS" {
                     total += 1
                 }
